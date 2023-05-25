fb3ecef8fff51a23212a3e74e109d5d66ea5a437
Student <Student@DESKTOP-VD2CFFR>
2023-05-25 20:16:23 +0900

    .

--- Mannada/src/main/java/kr/ac/kopo/mannada/controller/MannaController.java
+++ Mannada/src/main/java/kr/ac/kopo/mannada/controller/MannaController.java
@@ -2,8 +2,6 @@ package kr.ac.kopo.mannada.controller;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
