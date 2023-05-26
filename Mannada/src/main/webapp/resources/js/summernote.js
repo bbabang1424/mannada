485125e313fe8cfcfc5b0f4a8cee6ffa045d1ac1
ththd <ththd@kimsoyeon>
2023-05-26 23:26:24 +0900

    ..

--- Mannada/src/main/webapp/resources/js/summernote.js
+++ Mannada/src/main/webapp/resources/js/summernote.js
@@ -3,7 +3,7 @@
     $(document).ready(function() {
         //여기 아래 부분
         $('#summernote').summernote({
-            'width': '92%',
+            'width': '90%',
               height: 300,              // 에디터 높이
               minHeight: null,             // 최소 높이
               maxHeight: null,             // 최대 높이
@@ -12,6 +12,4 @@
               placeholder: '내용을 입력해주세요'	//placeholder 설정
               
         });
-        
-        // document.getElementById('currentDate').value = new Date().toISOString().substring(0, 10);
     });
