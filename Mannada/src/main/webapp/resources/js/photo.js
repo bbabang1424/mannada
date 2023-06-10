34dcf199e2e7207ae0d6fe48530df8b94813c18d
llnsn <llnsn@DESKTOP-Q5HDIOK>
2023-06-11 02:07:39 +0900

    마이페이지 탭 메뉴 수정

--- Mannada/src/main/webapp/resources/js/photo.js
+++ Mannada/src/main/webapp/resources/js/photo.js
@@ -1,4 +1,16 @@
-$("#file").on('change',function(){
-  var fileName = $("#file").val();
-  $(".upload-name").val(fileName);
-});
+const btn = document.getElementById('popupBtn');
+const modal = document.getElementById('modalWrap');
+const closeBtn = document.getElementById('closeBtn');
+
+btn.onclick = function() {
+  modal.style.display = 'block';
+}
+closeBtn.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
